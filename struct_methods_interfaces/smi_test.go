ccf6664663b190062dc66cb7a81a1232094eedd7
Anastasiya Latugovskaya <latugovskayawork@gmail.com>
2022-04-11 16:26:10 +0300

    refactor struct_methods_interfaces

--- struct_methods_interfaces/smi_test.go
+++ struct_methods_interfaces/smi_test.go
@@ -3,7 +3,8 @@ package smi
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,11 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %2.f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10, 10}
+		got := rectangle.Area()
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+
+	})
 
 }
