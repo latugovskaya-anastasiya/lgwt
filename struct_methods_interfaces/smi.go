b7d0b82751981f67d1d9e0c3c4ddec93c6420451
Anastasiya Latugovskaya <latugovskayawork@gmail.com>
2022-04-11 20:12:13 +0300

    refactor test struct_methods_interfaces

--- struct_methods_interfaces/smi.go
+++ struct_methods_interfaces/smi.go
@@ -25,7 +25,3 @@ func (c Circle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
-}
