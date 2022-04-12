aaa01f5591b0923f554b9c609aec82206322a7fb
Anastasiya Latugovskaya <latugovskayawork@gmail.com>
2022-04-12 13:10:06 +0300

    add pointers_errors

---
+++ pointers_errors/pointers_errors_test.go
@@ -0,0 +1,20 @@
+package pointersErrors
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	//	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d  want %d", got, want)
+	}
+}
