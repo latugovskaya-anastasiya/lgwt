ad0497304045424697cc9924c27252abf505fa3b
Kali <latugovskayawork@gmail.com>
2022-04-23 11:33:16 -0400

    refactor mocking

--- mocking/mock.go
+++ mocking/mock.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
 
 func main() {
 	Countdown(os.Stdout)
-}
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
 }
--- mocking/mock_test.go
+++ mocking/mock_test.go
@@ -9,9 +9,12 @@ func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 
 	Countdown(buffer)
-	got := buffer.String()
-	want := "3"
 
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
