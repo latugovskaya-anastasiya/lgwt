100de5d08c546564e121d6c8807c2dc4aeb1f667
Kali <latugovskayawork@gmail.com>
2022-04-23 17:36:16 -0400

    refactor

--- mocking/mock_test.go
+++ mocking/mock_test.go
@@ -70,15 +70,3 @@ type SpyTime struct {
 func (s *SpyTime) Sleep(duration time.Duration) {
 	s.durationSlept = duration
 }
-
-func TestConfigurableSleeper(t *testing.T) {
-	sleepTime := 5 * time.Second
-
-	spyTime := &SpyTime{}
-	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
-	sleeper.Sleep()
-
-	if spyTime.durationSlept != sleepTime {
-		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
-	}
-}
